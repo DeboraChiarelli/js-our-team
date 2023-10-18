e4b20105ec8e38ff1027fb499effa74ed0ba6136
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-10-18 15:13:40 +0200

    First Commit - add index.html/style.css/app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni:
+nome
+ruolo
+foto
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite. (come abbiamo fatto insieme in classe con l’array di studenti)
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe. */
