5f56797992f09579f9f26745749c1023549322eb
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-10-18 16:10:20 +0200

    Add - Array Membri Team

--- js/app.js
+++ js/app.js
@@ -9,3 +9,34 @@ MILESTONE 1:
 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe. */
+
+//- Creo array di oggetti 
+    //- determino: nome, ruolo, foto
+const membriTeam = {
+    nome : 'Angela Carroll',
+    ruolo : 'Chief Editor',
+    foto : 'angela-caroll-chief-editor.jpg',
+
+    nome : 'Angela Lopez',
+    ruolo : 'Social Media Manager',
+    foto : 'angela-lopez-social-media-manager.jpg',
+
+    nome : 'Barbara Ramos',
+    ruolo : 'Graphic Designer',
+    foto : 'barbara-ramos-graphic-designer.jpg',
+
+    nome : 'Scott Estrada',
+    ruolo : 'Developer',
+    foto : 'scott-estrada-developer.jpg',
+
+    nome : 'Walter Gordon',
+    ruolo : 'Office Manager',
+    foto : 'walter-gordon-office-manager.jpg',
+
+    nome : 'Wayne Barnett',
+    ruolo : 'Founder & CEO',
+    foto : 'wayne-barnett-founder-ceo.jpg',
+}
+
+
+
